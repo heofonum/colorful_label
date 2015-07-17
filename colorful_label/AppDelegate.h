d8c3c3682d21e5b83cfee96dcec79f9e17aeb3ca
HongjianZhu <hongjianzhu@HongjianZhudeiMac.local>
2015-07-17 11:53:00 +0800

    init

---
+++ colorful_label/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  colorful_label
+//
+//  Created by HongjianZhu on 15/7/17.
+//  Copyright (c) 2015年 HongjianZhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ colorful_label/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  colorful_label
+//
+//  Created by HongjianZhu on 15/7/17.
+//  Copyright (c) 2015年 HongjianZhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
